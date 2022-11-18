5447f7c147e37a9f81cf98f9142b1a360b8dce3c
MarcosGruah <110102063+MarcosGruah@users.noreply.github.com>
2022-11-18 19:29:42 -0300

    1012

---
+++ 1012/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _1012
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            double A = double.Parse(input[0]);
+            double B = double.Parse(input[1]);
+            double C = double.Parse(input[2]);
+
+            double areaTriangulo = (A * C) / 2;
+            double areaCirculo = 3.14159 * (C * C);
+            double areaTrapezio = ((A + B) * C) / 2;
+            double areaQuadrado = B * B;
+            double areaRetangulo = A * B;
+
+            Console.WriteLine($"TRIANGULO: {areaTriangulo:F3}");
+            Console.WriteLine($"CIRCULO: {areaCirculo:F3}");
+            Console.WriteLine($"TRAPEZIO: {areaTrapezio:F3}");
+            Console.WriteLine($"QUADRADO: {areaQuadrado:F3}");
+            Console.WriteLine($"RETANGULO: {areaRetangulo:F3}");
+
+        }
+    }
+}
