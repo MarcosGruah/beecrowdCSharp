c9fbb7e7092bf0bc656efd3b0becad3a264c097b
MarcosGruah <110102063+MarcosGruah@users.noreply.github.com>
2022-11-18 20:17:39 -0300

    1013 -> 1014

---
+++ 1013/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _1013
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int A = int.Parse(input[0]);
+            int B = int.Parse(input[1]);
+            int C = int.Parse(input[2]);
+
+            int highestNumberAB = (A + B + (Math.Abs(A - B))) / 2;
+            int highestNumberC = (C + highestNumberAB + (Math.Abs(C - highestNumberAB))) / 2;
+
+            Console.WriteLine($"{highestNumberC} eh o maior");
+
+        }
+    }
+}
