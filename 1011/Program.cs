cbc6fe2e5f7f2481ba8ed36e9223b652afd5e374
MarcosGruah <110102063+MarcosGruah@users.noreply.github.com>
2022-11-18 19:15:58 -0300

    1011

---
+++ 1011/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _1011
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double R = double.Parse(Console.ReadLine());
+            double PI = 3.14159;
+            double volumeEsfera = (4.0 / 3.0) * PI * (R * R * R);
+
+            Console.WriteLine($"VOLUME = {volumeEsfera:F3}");
+        }
+    }
+}
