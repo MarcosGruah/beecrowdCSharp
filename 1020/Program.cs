97ae9629afb89ef71f06153ffd4ad85435b7b85a
MarcosGruah <110102063+MarcosGruah@users.noreply.github.com>
2022-11-19 01:11:06 -0300

    1020 -> 1021

---
+++ 1020/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _1020
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            int ano = input / 365;
+            input %= 365;
+            int mes = input / 30;
+            input %= 30;
+            int dia = input;
+
+            Console.WriteLine($"{ano} ano(s)");
+            Console.WriteLine($"{mes} mes(es)");
+            Console.WriteLine($"{dia} dia(s)");
+
+        }
+    }
+}
---
+++ 1021/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _1021
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            double input = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("NOTAS:");
+            Console.WriteLine($"{(int)(input / 100)} nota(s) de R$ 100.00");
+            input = input % 100;
+            Console.WriteLine($"{(int)input / 50} nota(s) de R$ 50.00");
+            input = input % 50;
+            Console.WriteLine($"{(int)input / 20} nota(s) de R$ 20.00");
+            input = input % 20;
+            Console.WriteLine($"{(int)input / 10} nota(s) de R$ 10.00");
+            input = input % 10;
+            Console.WriteLine($"{(int)input / 5} nota(s) de R$ 5.00");
+            input = input % 5;
+            Console.WriteLine($"{(int)input / 2} nota(s) de R$ 2.00");
+            input = input % 2;
+
+            input *= 100;
+            Console.WriteLine("MOEDAS:");
+            Console.WriteLine($"{Math.Floor(input / 100)} moeda(s) de R$ 1.00");
+            input = input % 100;
+            Console.WriteLine($"{Math.Floor(input / 50)} moeda(s) de R$ 0.50");
+            input = input % 50;
+            Console.WriteLine($"{Math.Floor(input / 25)} moeda(s) de R$ 0.25");
+            input = input % 25;
+            Console.WriteLine($"{Math.Floor(input / 10)} moeda(s) de R$ 0.10");
+            input = input % 10;
+            Console.WriteLine($"{Math.Floor(input / 5)} moeda(s) de R$ 0.05");
+            input = input % 5;
+            Console.WriteLine($"{Math.Floor(input / 1)} moeda(s) de R$ 0.01");
+        }
+    }
+}
