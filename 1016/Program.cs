9f402909d1a2aa70ff2808dbffa1fe2dbb629f39
MarcosGruah <110102063+MarcosGruah@users.noreply.github.com>
2022-11-18 21:35:33 -0300

    1016

---
+++ 1016/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _1016
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1h Distancia = 30.
+            // 60min = Dist(30km)
+            // Xmin  = inputDistance
+
+            int inputDistance = int.Parse(Console.ReadLine());
+            int result = (60 * inputDistance) / 30;
+            Console.WriteLine($"{result} minutos");
+
+
+        }
+    }
+}
