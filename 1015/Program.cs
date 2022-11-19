848c33978a1e261b29278c8d800a202267efb485
MarcosGruah <110102063+MarcosGruah@users.noreply.github.com>
2022-11-18 21:07:07 -0300

    1015

---
+++ 1015/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _1015
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] point1 = Console.ReadLine().Split();
+            string[] point2 = Console.ReadLine().Split();
+
+            double distancia = Math.Sqrt(Math.Pow((double.Parse(point2[0]) - double.Parse(point1[0])), 2) + Math.Pow((double.Parse(point2[1]) - double.Parse(point1[1])), 2));
+
+            Console.WriteLine($"{distancia:F4}");
+
+
+        }
+    }
+}
