57d56b64a5424c700fdbfe0b4401e1367da4e9b4
MarcosGruah <110102063+MarcosGruah@users.noreply.github.com>
2022-11-28 18:56:04 -0300

    1046, 1047

--- 1046/Program.cs
+++ 1046/Program.cs
@@ -2,10 +2,24 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             int[] inputNumbers = Array.ConvertAll(Console.ReadLine().Split(), S => int.Parse(S));
+            DateTime startTime = new DateTime(2022, 01, 01, inputNumbers[0], 0, 0);
+            DateTime endTime;
 
+            if (inputNumbers[1] < inputNumbers[0])
+            {
+                endTime = new DateTime(2022, 01, 02, inputNumbers[1], 0, 0);
+            }
+            else
+            {
+                endTime = new DateTime(2022, 01, 01, inputNumbers[1], 0, 0);
+            }
+
+            TimeSpan matchDuration = endTime - startTime;
+
+            Console.WriteLine($"O JOGO DUROU {matchDuration.TotalHours} HORA(S)");
         }
     }
 }
---
+++ 1047/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _1046
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int[] inputNumbers = Array.ConvertAll(Console.ReadLine().Split(), S => int.Parse(S));
+            DateTime startTime = new DateTime(2022, 01, 01, inputNumbers[0], inputNumbers[1], 0);
+            DateTime endTime;
+
+            if (inputNumbers[2] < inputNumbers[0])
+            {
+                endTime = new DateTime(2022, 01, 02, inputNumbers[2], inputNumbers[3], 0);
+            }
+            else
+            {
+                endTime = new DateTime(2022, 01, 02, inputNumbers[2], inputNumbers[3], 0);
+            }
+
+            TimeSpan matchDuration = endTime - startTime;
+
+            Console.WriteLine($"O JOGO DUROU {(matchDuration.Days * 24) + matchDuration.Hours} HORA(S) E {matchDuration.Minutes} MINUTO(S)");
+        }
+    }
+}
