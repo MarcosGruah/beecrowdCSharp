1ebba4bfe8110495a49dca034d3689f43b3004c8
MarcosGruah <110102063+MarcosGruah@users.noreply.github.com>
2022-11-28 21:56:19 -0300

    1059, 1060

---
+++ 1059/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _1059
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            for (int i = 1; i < 101; i++)
+            {
+                if ((i % 2) == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ 1060/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _1060
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            double A = double.Parse(Console.ReadLine());
+            double B = double.Parse(Console.ReadLine());
+            double C = double.Parse(Console.ReadLine());
+            double D = double.Parse(Console.ReadLine());
+            double E = double.Parse(Console.ReadLine());
+            double F = double.Parse(Console.ReadLine());
+
+            double[] input = { A, B, C, D, E, F };
+            int count = 0;
+
+            foreach (var number in input)
+            {
+                if (number > 0)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine($"{count} valores positivos");
+        }
+    }
+}
